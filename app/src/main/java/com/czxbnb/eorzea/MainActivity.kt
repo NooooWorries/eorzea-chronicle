78525afac4e363a67966b86237d066c19704816b
Zixin Cheng <zixin.cheng@news.com.au>
2022-04-30 22:13:53 +1000

    Initial commit

---
+++ app/src/main/java/com/czxbnb/eorzea/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.czxbnb.eorzea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.czxbnb.eorzea.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
